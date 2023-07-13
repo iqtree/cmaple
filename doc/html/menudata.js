cd05c64e97f58729a40e240eaed1ff11d2998615
NhanLT <trongnhan.uit@gmail.com>
2023-07-13 17:21:35 +1000

    Add custom layout

--- doc/html/menudata.js
+++ doc/html/menudata.js
@@ -30,6 +30,4 @@ var menudata={children:[
 {text:"Class Members",url:"functions.html",children:[
 {text:"All",url:"functions.html"},
 {text:"Functions",url:"functions_func.html"},
-{text:"Variables",url:"functions_vars.html"}]}]},
-{text:"Files",url:"files.html",children:[
-{text:"File List",url:"files.html"}]}]}
+{text:"Variables",url:"functions_vars.html"}]}]}]}
