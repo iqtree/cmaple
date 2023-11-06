iqtree/cmaple

commit efa229d6574917057abacb89c14e5382154d0d8c

Integrate bug fixed to avoid infinite loop when optimizing tree topology

tree/phylonode.cpp
@@ -47,6 +47,16 @@ void PhyloNode::setOutdated(bool new_outdated)
     outdated_ = new_outdated;
 }
 
+const bool PhyloNode::isSPRApplied() const
+{
+    return spr_applied_;
+}
+
+void PhyloNode::setSPRApplied(bool spr_applied)
+{
+    spr_applied_ = spr_applied;
+}
+
 RealNumType PhyloNode::getUpperLength() const
 {
     return length_;

tree/phylonode.h
@@ -57,7 +57,11 @@ private:
     const bool is_internal_;
     
     /// flag to avoid traversing a clade multiple times during topology optimization
+    /// TRUE if the likelihoods were updated due to some SPR moves
     bool outdated_;
+    
+    // TRUE if we applied SPR move on the upper branch of this node
+    bool spr_applied_;
     
     // branch length
     float length_ = 0; // using float allows it to fit into the 6 bytes padding after the two bools.
@@ -85,7 +89,7 @@ private:
     
     /** move constructor */
     PhyloNode(PhyloNode&& node): is_internal_{node.isInternal()}, data_(std::move(node.getNode()), is_internal_),
-    other_lh_(std::move(node.getOtherLh())), outdated_(node.isOutdated()), length_(node.getUpperLength()) {};
+    other_lh_(std::move(node.getOtherLh())), outdated_(node.isOutdated()), spr_applied_(node.isSPRApplied()), length_(node.getUpperLength()) {};
     
     /** destructor */
     ~PhyloNode()
@@ -135,6 +139,16 @@ private:
         Set outdated_
      */
     void setOutdated(bool new_outdated);
+    
+    /**
+        Get spr_applied_
+     */
+    const bool isSPRApplied() const;
+    
+    /**
+        Set spr_applied_
+     */
+    void setSPRApplied(bool spr_applied);
     
     /**
         Get length of the upper branch

tree/tree.cpp
@@ -1225,6 +1225,9 @@ void Tree::seekSubTreePlacement(Index& best_node_index, RealNumType &best_lh_dif
 
 void Tree::applySPR(const Index subtree_index, PhyloNode& subtree, const Index best_node_index, const bool is_mid_branch, const RealNumType branch_length, const RealNumType best_lh_diff)
 {
+    // record the SPR applied at this subtree
+    subtree.setSPRApplied(true);
+
     // remove subtree from the tree
     const Index parent_index = subtree.getNeighborIndex(TOP);
     PhyloNode& parent_subtree = nodes[parent_index.getVectorIndex()]; // subtree->neighbor->getTopNode();
@@ -2767,7 +2770,7 @@ void Tree::refreshAllNonLowerLhs()
     }
 }
 
-void Tree::setAllNodeOutdated()
+void Tree::resetSPRFlags(const bool reset_outdated)
 {
     // start from the root
     stack<NumSeqsType> node_stack;
@@ -2781,7 +2784,9 @@ void Tree::setAllNodeOutdated()
         node_stack.pop();
         
         // set the current node outdated
-        node.setOutdated(true);
+        if (reset_outdated)
+            node.setOutdated(true);
+        node.setSPRApplied(false);
         
         // traverse downward
         /*for (Index neighbor_index:node.getNeighborIndexes(TOP))
@@ -2824,7 +2829,7 @@ RealNumType Tree::improveEntireTree(bool short_range_search)
         }
         
         // only process outdated node to avoid traversing the same part of the tree multiple times
-        if (node.isOutdated())
+        if (node.isOutdated() && !node.isSPRApplied())
         {
             node.setOutdated(false);
             

maple/cmaple.cpp
@@ -180,7 +180,7 @@ void CMaple::optimizeTreeTopology(bool short_range_search)
     for (int i = 0; i < num_tree_improvement; ++i)
     {
         // first, set all nodes outdated
-        tree.setAllNodeOutdated();
+        tree.resetSPRFlags(true);
         
         // traverse the tree from root to try improvements on the entire tree
         RealNumType improvement = tree.improveEntireTree(short_range_search);
@@ -195,6 +195,9 @@ void CMaple::optimizeTreeTopology(bool short_range_search)
         // run improvements only on the nodes that have been affected by some changes in the last round, and so on
         for (int j = 0; j < 20; ++j)
         {
+            // forget SPR_applied flag to allow new SPR moves
+            tree.resetSPRFlags(false);
+            
             improvement = tree.improveEntireTree(short_range_search);
             cout << "Tree was improved by " + convertDoubleToString(improvement) + " at subround " + convertIntToString(j + 1) << endl;
             
@@ -220,7 +223,7 @@ void CMaple::optimizeBranchLengthsOfTree()
     cout << "Start optimizing branch lengths" << endl;
     
     // first, set all nodes outdated
-    tree.setAllNodeOutdated();
+    tree.resetSPRFlags(true);
    
     // traverse the tree from root to optimize branch lengths
     PositionType num_improvement = tree.optimizeBranchLengths();

tree/tree.h
@@ -419,9 +419,11 @@ public:
     void refreshAllLhs();
     
     /**
-        Traverse the tree to set the outdated flag (which is used to prevent traversing the same part of the tree multiple times) of all nodes to TRUE
+        Traverse the tree to reset the outdated/spr_applied flags (which is used to prevent traversing the same part of the tree multiple times) of all nodes
+        outdated = true;
+        spr_applied = false;
      */
-    void setAllNodeOutdated();
+    void resetSPRFlags(const bool reset_outdated);
     
     /**
         Try to improve the entire tree with SPR moves

commit 793cd68fb0dc0ea9f30dc423af8bdb662c205b7f

Bug fixed caused by the commit "efa229d"

tree/phylonode.cpp
@@ -55,14 +55,14 @@ void cmaple::PhyloNode::setOutdated(bool new_outdated)
     outdated_ = new_outdated;
 }
 
-const bool cmaple::PhyloNode::isSPRApplied() const
+const uint8_t cmaple::PhyloNode::getSPRCount() const
 {
-    return spr_applied_;
+    return spr_count_;
 }
 
-void cmaple::PhyloNode::setSPRApplied(bool spr_applied)
+void cmaple::PhyloNode::setSPRCount(uint8_t spr_count)
 {
-    spr_applied_ = spr_applied;
+    spr_count_ = spr_count;
 }
 
 RealNumType cmaple::PhyloNode::getUpperLength() const

tree/phylonode.h
@@ -60,8 +60,8 @@ namespace cmaple
         /// TRUE if the likelihoods were updated due to some SPR moves
         bool outdated_;
         
-        // TRUE if we applied SPR move on the upper branch of this node
-        bool spr_applied_;
+        // Count the number of SPR moves applied at the upper branch of this node
+        uint8_t spr_count_ = 0;
         
         // branch length
         float length_ = 0; // using float allows it to fit into the 6 bytes padding after the two bools.
@@ -78,14 +78,14 @@ namespace cmaple
         PhyloNode() = delete;
         
         /** constructor */
-        PhyloNode(LeafNode&& leaf): is_internal_{false}, outdated_{true}, spr_applied_{false}, data_(std::move(leaf)) {};
+        PhyloNode(LeafNode&& leaf): is_internal_{false}, outdated_{true}, spr_count_{0}, data_(std::move(leaf)) {};
         
         /** constructor */
-        PhyloNode(InternalNode&& internal) noexcept: is_internal_{true}, outdated_{true}, spr_applied_{false}, data_(std::move(internal)) {};
+        PhyloNode(InternalNode&& internal) noexcept: is_internal_{true}, outdated_{true}, spr_count_{0}, data_(std::move(internal)) {};
         
         /** move constructor */
         PhyloNode(PhyloNode&& node) noexcept : is_internal_{node.is_internal_}, data_(std::move(node.data_), node.is_internal_),
-        other_lh_(std::move(node.other_lh_)), outdated_(node.outdated_), spr_applied_(node.spr_applied_), length_(node.length_) {};
+        other_lh_(std::move(node.other_lh_)), outdated_(node.outdated_), spr_count_(node.spr_count_), length_(node.length_) {};
         
         /** destructor */
         ~PhyloNode()
@@ -140,14 +140,14 @@ namespace cmaple
         void setOutdated(bool new_outdated);
         
         /**
-         Get spr_applied_
+         Get spr_count_
          */
-        const bool isSPRApplied() const;
+        const uint8_t getSPRCount() const;
         
         /**
-         Set spr_applied_
+         Set spr_count_
          */
-        void setSPRApplied(bool spr_applied);
+        void setSPRCount(uint8_t spr_count);
         
         /**
          Get length of the upper branch

tree/tree.cpp
@@ -2177,7 +2177,7 @@ template <const StateType num_states>
 void cmaple::Tree::applyOneSPR(const Index subtree_index, PhyloNode& subtree, const Index best_node_index, const bool is_mid_branch, const RealNumType branch_length, const RealNumType best_lh_diff)
 {
     // record the SPR applied at this subtree
-    subtree.setSPRApplied(true);
+    subtree.setSPRCount(subtree.getSPRCount() + 1);
     // remove subtree from the tree
     const Index parent_index = subtree.getNeighborIndex(TOP);
     PhyloNode& parent_subtree = nodes[parent_index.getVectorIndex()]; // subtree->neighbor->getTopNode();
@@ -3678,7 +3678,7 @@ RealNumType cmaple::Tree::improveEntireTree(bool short_range_search)
         }
         
         // only process outdated node to avoid traversing the same part of the tree multiple times
-        if (node.isOutdated() && !node.isSPRApplied())
+        if (node.isOutdated() && node.getSPRCount() <= 5)
         {
             node.setOutdated(false);
             
@@ -7215,7 +7215,7 @@ void cmaple::Tree::resetSPRFlags(const bool n_SPR_applied, const bool update_out
         PhyloNode& node = nodes[i];
         
         // update SPR_applied
-        node.setSPRApplied(n_SPR_applied);
+        node.setSPRCount(0);
         
         // update outdated if necessary
         if (update_outdated)