c078afff0e048d15cdac191ea160f70d430b40be
NhanLT <trongnhan.uit@gmail.com>
2024-12-06 11:57:22 +1100

    Revert "Fix unit tests"
    This reverts commit eaa23ea78cd0513e22260c704c91320719acc45e.

--- unittest/model_test.cpp
+++ unittest/model_test.cpp
@@ -11,22 +11,22 @@ TEST(Model, initMutationMat)
     ModelDNA model_JC = ModelDNA(cmaple::ModelBase::JC);
     
     EXPECT_EQ(model_JC.sub_model, cmaple::ModelBase::JC);
-    EXPECT_EQ(model_JC.getRootFreq(1), 0.25);
-    EXPECT_EQ(model_JC.getRootFreq(3), 0.25);
-    EXPECT_EQ(model_JC.getInverseRootFreq(0), 4);
-    EXPECT_EQ(model_JC.getInverseRootFreq(2), 4);
-    EXPECT_EQ(model_JC.getRootLogFreq(0), log(0.25));
-    EXPECT_EQ(model_JC.getRootLogFreq(3), model_JC.getRootLogFreq(0));
-    EXPECT_EQ(model_JC.getDiagonalMutationMatrixEntry(1,0), -1);
-    EXPECT_EQ(model_JC.getDiagonalMutationMatrixEntry(2,0), -1);
-    EXPECT_EQ(model_JC.getMutationMatrix(0)[5], -1);
-    EXPECT_EQ(model_JC.getMutationMatrix(0)[10], -1);
-    EXPECT_EQ(model_JC.getTransposedMutationMatrix(0)[0], -1);
-    EXPECT_EQ(model_JC.getTransposedMutationMatrix(0)[15], -1);
-    EXPECT_EQ(model_JC.getFreqiFreqjQij(0, 5), -1);
-    EXPECT_EQ(model_JC.getFreqiFreqjQij(0,10), -1);
-    EXPECT_EQ(model_JC.getFreqjTransposedijRow(0)[0], -0.25);
-    EXPECT_EQ(model_JC.getFreqjTransposedijRow(0)[15], -0.25);
+    EXPECT_EQ(model_JC.root_freqs[1], 0.25);
+    EXPECT_EQ(model_JC.root_freqs[3], 0.25);
+    EXPECT_EQ(model_JC.inverse_root_freqs[0], 4);
+    EXPECT_EQ(model_JC.inverse_root_freqs[2], 4);
+    EXPECT_EQ(model_JC.root_log_freqs[0], log(0.25));
+    EXPECT_EQ(model_JC.root_log_freqs[3], model_JC.root_log_freqs[0]);
+    EXPECT_EQ(model_JC.diagonal_mut_mat[1], -1);
+    EXPECT_EQ(model_JC.diagonal_mut_mat[2], -1);
+    EXPECT_EQ(model_JC.mutation_mat[5], -1);
+    EXPECT_EQ(model_JC.mutation_mat[10], -1);
+    EXPECT_EQ(model_JC.transposed_mut_mat[0], -1);
+    EXPECT_EQ(model_JC.transposed_mut_mat[15], -1);
+    EXPECT_EQ(model_JC.freqi_freqj_qij[5], -1);
+    EXPECT_EQ(model_JC.freqi_freqj_qij[10], -1);
+    EXPECT_EQ(model_JC.freq_j_transposed_ij[0], -0.25);
+    EXPECT_EQ(model_JC.freq_j_transposed_ij[15], -0.25);
 }
 
 // NOT YET DONE
