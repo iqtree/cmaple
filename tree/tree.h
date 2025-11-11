f80eb458a2367f6563c1678b0aa698cf2d37e621
NhanLT <trongnhan.uit@gmail.com>
2025-11-11 15:40:16 +0000

    update/make local reference when placing a new sample at the root

--- tree/tree.h
+++ tree/tree.h
@@ -2143,7 +2143,7 @@ bool isDiffFromOrigPlacement(
      Make a local reference at a node
      */
     template <const StateType num_states>
-    auto makeReferenceNode(PhyloNode& node, const cmaple::Index node_index,
+    auto makeReferenceNode(PhyloNode& node, const NumSeqsType& node_vec_index,
             const int old_num_desc) -> void;
 };
 
